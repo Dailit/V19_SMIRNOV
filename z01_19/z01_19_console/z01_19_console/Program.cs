3de3ea5588dde88138ef043a916b25b7ca2ad21c
Dailit <95355824+Dailit@users.noreply.github.com>
2021-12-01 18:03:38 +0300

    added z01

---
+++ z01_19/z01_19_console/z01_19_console/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace v19
+{
+    class v19
+    {
+        static void Summ()
+        {
+            Console.Write("Первый член геометрической прогрессии: ");
+            float b1 = float.Parse(Console.ReadLine());
+            Console.Write("Знаменатель: ");
+            float q = float.Parse(Console.ReadLine());
+            Console.Write("Число членов прогрессии: ");
+            int n = int.Parse(Console.ReadLine());
+            double qn = 0;
+            double s = 0;
+            if (q != 1)
+            {
+                if (q > 0)
+                    qn = Math.Exp(Math.Log(q) * n);
+                else
+                    if (q < 0)
+                        qn = -Math.Exp(Math.Log(-q) * n);
+                    else
+                        qn = 0;
+                s = b1 * (1 - qn) / (1 - q);
+            }
+            Console.WriteLine("Сумма членов геометрической прогрессии: " + s);
+        }
+        static void Equilateral()
+        {
+            Console.Write("a = ");
+            float a = float.Parse(Console.ReadLine());
+            Console.Write("b = ");
+            float b = float.Parse(Console.ReadLine());
+            Console.Write("c = ");
+            float c = float.Parse(Console.ReadLine());
+            if (a == b && b == c)
+                Console.WriteLine("Треугольник равносторонний");
+            else
+                Console.WriteLine("Треугольник не равносторонний");
+        }
+
+        static void Main()
+        {
+            Console.Write("Номер задания(1,2): ");
+            int n = int.Parse(Console.ReadLine());
+            if (n == 1)
+                Summ();
+            if (n == 2)
+                Equilateral();
+        }
+    }
+}
---
+++ z01_19/z01_19_forms/z01_19_forms/Form2.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace z01_19_forms
+{
+    public partial class Form2 : Form
+    {
+        private float a;
+        private float b;
+        private float c;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        private string Equilateral()
+        {
+            string result;
+            if (float.TryParse(textBox1.Text, out a) && float.TryParse(textBox2.Text, out b) && float.TryParse(textBox3.Text, out c))
+            {
+                if (a == b && b == c)
+                    result = "Треугольник равносторонний";
+                else
+                    result = "Треугольник не равносторонний";
+            }
+            else
+                result = "Некорректные данные";
+            return result;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+        }
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label5.Text = Equilateral();
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+        }
+        private void label3_Click(object sender, EventArgs e)
+        {
+        }
+        private void label4_Click(object sender, EventArgs e)
+        {
+        }
+        private void label5_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
